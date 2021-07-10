00900093b00e320a0990768da301838b41c6ec22
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-07-10 00:23:45 -0300

    refactor: reestruturação da pasta app

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 
-import GlobalStyles from './assets/styles/global';
-import defaultTheme from './assets/styles/themes/default';
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
 
 function App() {
   return (
