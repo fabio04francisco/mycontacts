f8df7404fd9ee2657bc067d11d46f59e5a0b9c37
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-07-14 00:04:26 -0300

    feat: add page Home

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,91 @@
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arow" />
+          </button>
+        </header>
+      </ListContainer>
+
+      <Card>
+        <div className="infor">
+          <div className="contact-name">
+            <strong>Fábio Francisco</strong>
+            <small>intagram</small>
+          </div>
+          <span>fabinhofrancisco@gmail.com</span>
+          <span>(48) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+      <Card>
+        <div className="infor">
+          <div className="contact-name">
+            <strong>Fábio Francisco</strong>
+            <small>intagram</small>
+          </div>
+          <span>fabinhofrancisco@gmail.com</span>
+          <span>(48) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+      <Card>
+        <div className="infor">
+          <div className="contact-name">
+            <strong>Fábio Francisco</strong>
+            <small>intagram</small>
+          </div>
+          <span>fabinhofrancisco@gmail.com</span>
+          <span>(48) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+
+    </Container>
+  );
+}
