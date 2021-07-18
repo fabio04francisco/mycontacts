947e6d94cce1d38867ebf16ea385b072a2a45b06
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-07-17 22:26:07 -0300

    feat: add estrutura do formulário de contato

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,9 +1,16 @@
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
 
 export default function EditContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+
+      <ContactForm
+        buttonLabel="Cadastrar"
+      />
+    </>
   );
 }
