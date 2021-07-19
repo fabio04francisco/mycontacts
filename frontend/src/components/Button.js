f1459b279db7c58fd30ca936144b7b07fa9160ad
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-07-18 23:22:21 -0300

    feat: add tema danger e lagura flexivel

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.button`
-  width: 100%;
   height: 52px;
   border: none;
+  padding: 0 16px;
   background: ${({ theme }) => theme.colors.primary.main};
   font-size: 16px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
@@ -11,6 +11,7 @@ export default styled.button`
   color: #fff;
   border-radius: 4px;
   transition: background 0.2s ease-in;
+
   &:hover {
     background: ${({ theme }) => theme.colors.primary.light};
   }
@@ -23,6 +24,18 @@ export default styled.button`
     background: #CCC;
     cursor: default;
   }
+
+  ${({ theme, danger }) => danger && css`
+    background: ${theme.colors.danger.main};
+
+    &:hover {
+      background: ${theme.colors.danger.light};
+    }
+
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
+  `}
 
 
 `;
