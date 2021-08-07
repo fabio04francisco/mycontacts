ba75c75d1c0662716f3e6e969bc70c278f787c6d
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-08-06 23:17:58 -0300

    feat: implementado metodo store

--- api/src/app/controllers/ContactController.js
+++ api/src/app/controllers/ContactController.js
@@ -3,7 +3,6 @@ const ContactsRepository = require('../repositories/ContactsRepository');
 class ContactController {
   async index(request, response) {
     const contacts = await ContactsRepository.findAll();
-
     response.json(contacts);
   }
 
@@ -19,8 +18,24 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
-    // cria um contato
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required.' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail is already been taken' });
+    }
+
+    const contact = await ContactsRepository.create(name, email, phone, category_id);
+
+    response.json(contact);
   }
 
   update() {
