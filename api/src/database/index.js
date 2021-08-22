4e4371f9809f980d57293dc60211ef8d7236ceda
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-08-22 09:53:18 -0300

    feat: cria conexão com banco postgre

---
+++ api/src/database/index.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'mycontacts',
+});
+
+client.connect();
