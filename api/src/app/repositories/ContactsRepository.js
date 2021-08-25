8a8b01cd16535ec766efc0b510ecdc7ca8ffb3ce
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-08-24 22:40:09 -0300

    feat: add nome categoria no retorno dos contatos.

--- api/src/app/repositories/ContactsRepository.js
+++ api/src/app/repositories/ContactsRepository.js
@@ -3,12 +3,29 @@ const db = require('../../database');
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
-    const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
+    const rows = await db.query(`
+      SELECT
+        contacts.*, categories.name AS categosry_name
+      FROM
+        contacts
+      LEFT JOIN
+        categories ON categories.id = contacts.category_id
+      ORDER BY name ${direction}
+    `);
     return rows;
   }
 
   async findById(id) {
-    const [rows] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    const [rows] = await db.query(`
+      SELECT
+        contacts.*, categories.name AS categosry_name
+      FROM
+        contacts
+      LEFT JOIN
+        categories ON categories.id = contacts.category_id
+      WHERE
+        contacts.id = $1
+      `, [id]);
     return rows;
   }
 
