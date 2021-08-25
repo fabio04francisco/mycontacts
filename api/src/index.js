69c1ab9018d6ff7cf32594b62c1c2c15dc30e974
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-08-24 23:48:23 -0300

    feat: tratamento de erros por handler express

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('##### Error handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('API started at http://localhost:3000'));
