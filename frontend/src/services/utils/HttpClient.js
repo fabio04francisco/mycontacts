01cfb40138c55263d854fe9b39a51fb6a1f17976
Fabio Francisco <fabinhofrancisco@gmail.com>
2022-01-18 23:01:31 -0300

    feat: parse do body baseado no Content-Type

--- frontend/src/services/utils/HttpClient.js
+++ frontend/src/services/utils/HttpClient.js
@@ -6,11 +6,23 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
-
     await delay(500);
 
-    return response.json();
+    const response = await fetch(`${this.baseURL}${path}`);
+
+    let body = null;
+    const contentType = response.headers.get('Content-Type');
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`,
+    );
   }
 }
 
